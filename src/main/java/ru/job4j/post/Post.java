0e3a74225ebb43d721b0027d7d778bf1a8e1c649
tigrdima <duys79yandex.ru>
2022-04-27 11:02:38 +0300

    2.2. Модель данных - Post. [#285211]

---
+++ src/main/java/ru/job4j/post/Post.java
@@ -0,0 +1,101 @@
+package ru.job4j.post;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Post {
+    private int id;
+    private String title;
+    private String link;
+    private String description;
+    private LocalDateTime created;
+
+    public Post(int id, String title, String link, String description, LocalDateTime created) {
+        this.id = id;
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.created = created;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    @Override
+    public String toString() {
+        return "Post{"
+                + "id=" + id
+                + ", title='" + title + '\''
+                + ", link='" + link + '\''
+                + ", description='" + description + '\''
+                + ", created=" + created
+                + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Post post = (Post) o;
+        return  title.equals(post.title) && link.equals(post.link);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, link);
+    }
+
+    public static void main(String[] args) {
+        Post post1 = new Post(1, "Job1", "link1", "блаблабла", LocalDateTime.now());
+        Post post2 = new Post(2, "Job1", "link1", "блаблаблабла", LocalDateTime.now());
+
+        boolean rsl = post1.equals(post2);
+        boolean rsl1 = post1.getDescription().equals(post2.getDescription());
+
+        System.out.println(rsl);
+        System.out.println(rsl1);
+    }
+}
+
+
