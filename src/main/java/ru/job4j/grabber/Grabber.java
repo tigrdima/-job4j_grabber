7014ac837aa4848a909eec1c383069582351753e
tigrdima <duys79yandex.ru>
2022-04-28 13:40:47 +0300

    5. PsqlStore [#285209 #294656]

---
+++ src/main/java/ru/job4j/grabber/Grabber.java
@@ -0,0 +1,75 @@
+package ru.job4j.grabber;
+
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+import ru.job4j.utils.HarbCareerDateTimeParser;
+
+import java.io.*;
+import java.util.List;
+import java.util.Properties;
+
+import static org.quartz.JobBuilder.newJob;
+import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
+import static org.quartz.TriggerBuilder.newTrigger;
+
+public class Grabber implements Grab {
+    private final Properties cfg = new Properties();
+
+    public Store store() {
+        return new PsqlStore(cfg);
+    }
+
+    public Scheduler scheduler() throws SchedulerException {
+        Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+        scheduler.start();
+        return scheduler;
+    }
+
+    public void cfg() throws IOException {
+        try (InputStream in = Grabber.class.getClassLoader().getResourceAsStream("aggregator_vacancy.properties")) {
+            cfg.load(in);
+        }
+    }
+
+    @Override
+    public void init(Parse parse, Store store, Scheduler scheduler) throws SchedulerException {
+        JobDataMap data = new JobDataMap();
+        data.put("store", store);
+        data.put("parse", parse);
+        JobDetail job = newJob(GrabJob.class)
+                .usingJobData(data)
+                .build();
+        SimpleScheduleBuilder times = simpleSchedule()
+                .withIntervalInSeconds(Integer.parseInt(cfg.getProperty("time")))
+                .repeatForever();
+        Trigger trigger = newTrigger()
+                .startNow()
+                .withSchedule(times)
+                .build();
+        scheduler.scheduleJob(job, trigger);
+    }
+
+    public static class GrabJob implements Job {
+
+        @Override
+        public void execute(JobExecutionContext context) {
+            System.out.println("saving.....");
+            JobDataMap map = context.getJobDetail().getJobDataMap();
+            Store store = (Store) map.get("store");
+            Parse parse = (Parse) map.get("parse");
+            List<Post> postList = parse.list("/vacancies/java_developer?page=");
+            for (Post p : postList) {
+                store.save(p);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Grabber grab = new Grabber();
+        grab.cfg();
+        Scheduler scheduler = grab.scheduler();
+        Store store = grab.store();
+        grab.init(new HabrCareerParse(new HarbCareerDateTimeParser()), store, scheduler);
+    }
+
+}
--- src/main/java/ru/job4j/grabber/Parse.java
+++ src/main/java/ru/job4j/grabber/Parse.java
@@ -1,6 +1,5 @@
 package ru.job4j.grabber;
 
-import java.io.IOException;
 import java.util.List;
 
 public interface Parse {
